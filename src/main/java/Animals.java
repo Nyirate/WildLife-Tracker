d1c664a2053c38e0ef44f207b3b75951386564a5
Nyirate <rwakaemma34@gmail.com>
2019-09-21 02:24:34 +0200

    add a hbs

---
+++ src/main/java/Animals.java
@@ -0,0 +1,21 @@
+import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Animal {
+    public String name;
+    public int id;
+
+    public Animal(String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
