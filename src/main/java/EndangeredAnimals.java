258997cd9889ff5dd4c7822357d7ab3b3b3503c4
Nyirate <rwakaemma34@gmail.com>
2019-09-21 03:00:57 +0200

    add new codes

--- src/main/java/EndangeredAnimals.java
+++ src/main/java/EndangeredAnimals.java
@@ -1,2 +1,28 @@
+import org.sql2o.Connection;
+
+import java.util.List;
+
 public class EndangeredAnimals {
+    public int id;
+    public  String health;
+    public String age;
+
+
+    public EndangeredAnimals(int id, String health, String age) {
+        this.id = id;
+        this.health = health;
+        this.age = age;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+    public String getAge() {
+        return age;
+    }
 }
